563905d84c3833ec591740f2cb2c549e10caf936
Peter-32 <peter@impactradius.com>
2021-02-27 17:59:26 -0800

    a

--- app/main.py
+++ app/main.py
@@ -13,7 +13,7 @@ def index():
 @app.route('/setup')
 def setup():
     setup = {'earnings': "2100",
-             'purchase1': "", 'cost1': "", 'category1': "need",
+             'purchase1': "1", 'cost1': "1", 'category1': "want",
              'purchase2': "", 'cost2': "", 'category2': "need",
              'purchase3': "", 'cost3': "", 'category3': "need",
              'purchase4': "", 'cost4': "", 'category4': "need",
@@ -27,7 +27,7 @@ def setup():
              'purchase12': "", 'cost12': "", 'category12': "need",
              'saving': ""
              }
-    return render_template("setup.html", setup)
+    return render_template("setup.html", setup=setup)
 
 
 
